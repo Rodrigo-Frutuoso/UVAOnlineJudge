3cb0b7eefcea235351050ec22ea7857d9bf7b07d
Rodrigo-Frutuoso <153727090+Rodrigo-Frutuoso@users.noreply.github.com>
2024-05-09 18:52:01 +0100

    Add files via upload

---
+++ Club/src/Event_Planning_11559/Main.java
@@ -0,0 +1,32 @@
+package Event_Planning_11559;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int numParticipants = sc.nextInt();
+            int budget = sc.nextInt();
+            int numHotels = sc.nextInt();
+            int numWeeks = sc.nextInt();
+            int minCost = Integer.MAX_VALUE;
+            for (int i = 0; i < numHotels; i++) {
+                int pricePerPerson = sc.nextInt();
+                for (int j = 0; j < numWeeks; j++) {
+                    int numBedsAvailable = sc.nextInt();
+                    if (numBedsAvailable >= numParticipants) {
+                        minCost = Math.min(minCost, pricePerPerson * numParticipants);
+                    }
+                }
+            }
+            if (minCost <= budget) {
+                System.out.println(minCost);
+            } 
+            else {
+                System.out.println("stay home");
+            }
+        }
+        sc.close();
+    }
+}
